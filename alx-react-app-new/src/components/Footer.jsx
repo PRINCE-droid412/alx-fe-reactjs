95b419a705c06806a47848d239f473b3a4f84539
PRINCE-droid412 <Princetakyijnr@yahoo.com>
2024-11-09 03:50:05 -1200

    alx react app new files added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: "#333", color: "#fff", Padding: "10px 20px", textAlign: "center", fontsize: "14px", marginTop: "20px", }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+    return (
+        <main style={{
+            border: "2px solid #333", padding: "20px", margin: "10px", color: "#555", fontWeight: "bold", backgroundColor: "#f9f9f9"
+        }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
