65a2721b93cf4a24a3b8c5260a0a3b6f43a6ecb9
PRINCE <Princetakyijnr@yahoo.com>
2024-11-10 16:12:14 -1200

    components bugs fixed

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <p style={{textAlign:'center', background:'lightBlue'}}>© 2023 Favorite Tech Company</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div style={{ padding: '20px'  }}>
+            <h1 style={{fontSize:'40px', textAlign:'center', padding:'20px', background:'grey'}}>Welcome to Our Company</h1>
+            <p style={{textAlign:'center'}}>We are dedicated to delivering excellence in all our services.</p>
+        </div>
+    );
+}
+
+export default Home;
