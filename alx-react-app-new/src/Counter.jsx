d845e7691e4649e2f1a9536e1625dc6c3e075edf
PRINCE <Princetakyijnr@yahoo.com>
2024-11-10 16:22:04 -1200

    Update Counter.jsx

--- alx-react-app-new/src/Counter.jsx
+++ alx-react-app-new/src/Counter.jsx
@@ -1,16 +1,18 @@
+import React from 'react'
 import { useState } from 'react'
 
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>Current Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
-  );
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>
+                Counter
+            </p>
+            <p>Counter:{count}</p>   
+            {/* this will display the counts  */}
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    )
 }
-
-export default Counter;
