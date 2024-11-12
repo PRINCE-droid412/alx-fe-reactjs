6dc8bc9e2352499b82b19032a0c765631563eda3
PRINCE <Princetakyijnr@yahoo.com>
2024-11-11 13:01:06 -1200

    components files updated

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const UserProfile = (props) => {
+    return (
+        <div className='User-Profile'>
+            <h2>
+                {props.name}
+            </h2>
+
+            <p>
+                Age:{props.age}
+            </p>
+
+            <p>Bio:{props.bio}
+            </p>
+        </div>
+    )
+}
+
+export default UserProfile
