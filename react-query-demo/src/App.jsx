86616e5a07a0f3fcea5676e21ff88ededdb777c1
PRINCE <Princetakyijnr@yahoo.com>
2024-11-21 14:44:39 -1200

    New files updated

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: '20px' }}>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
