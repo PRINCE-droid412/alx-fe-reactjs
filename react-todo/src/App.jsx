444bd7640d29d8e1704f0ff97155e7a0c74150c0
PRINCE <Princetakyijnr@yahoo.com>
2024-11-23 16:08:56 -1200

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import TodoList from './components/TodoList';
+import React from "react";
+import TodoList from "./components/TodoList";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <TodoList />
-    </div>
+    <>
+      <div>
+        <TodoList />
+      </div>
+    </>
   );
 }
 
