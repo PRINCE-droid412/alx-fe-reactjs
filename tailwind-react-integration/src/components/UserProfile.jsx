65ce3d021c394ae41279513382fd1e1eacbe6ba9
PRINCE <Princetakyijnr@yahoo.com>
2024-11-28 14:37:32 -1200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,7 +9,7 @@ function UserProfile() {
         <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 hover:text-blue-500 my-4 transition-colors duration-300 ease-in-out">
           John Doe
         </h1>
-        <p className="text-sm sm:text-base md:text-lg text-gray-600">
+        <p className="text-sm sm:text-base md:text-xl text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
